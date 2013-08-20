d14b4bc0ab19e5b63f797866c8687cf4ac34356d
Colden Cullen <coldencullen@gmail.com>
2013-08-20 18:16:26 -0400

    Added Xcode project structure. Does not build in Xcode!

---
+++ Game/Project-192.OSX/Project-192.OSX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project-192.OSX
+//
+//  Created by Colden Cullen on 8/20/13.
+//  Copyright (c) 2013 Studio 192. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
