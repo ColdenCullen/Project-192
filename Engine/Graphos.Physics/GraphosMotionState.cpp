ad2691da1e0ba65cdd85effe01bed36eeb3921b8
BrandonLittell <brandonl3737@gmail.com>
2013-11-02 21:39:05 -0400

    Added new physics classes.

---
+++ Engine/Graphos.Physics/GraphosMotionState.cpp
@@ -0,0 +1,33 @@
+#include "GraphosMotionState.h"
+#include "GameObject.h"
+
+using namespace Graphos::Core;
+using namespace Graphos::Physics;
+
+GraphosMotionState::GraphosMotionState( GameObject* owner ) : IComponent( owner ) {}
+
+void Graphos::Physics::GraphosMotionState::Shutdown( void )
+{
+}
+
+void Graphos::Physics::GraphosMotionState::getWorldTransform( btTransform& worldTrans ) const
+{
+	worldTrans.setFromOpenGLMatrix( owner->transform->WorldMatrix().dataArray );
+
+
+}
+
+void Graphos::Physics::GraphosMotionState::setWorldTransform( const btTransform& worldTrans )
+{
+	/*
+	// Update position of object
+	btVector3 tempOrigin = worldTrans.getOrigin();
+	owner->transform->TranslateTo( tempOrigin.x(), tempOrigin.y(), tempOrigin.z() );
+	*/
+
+	// TODO: Update position/rotation/scale
+	// TODO: FIX FOR RUNTIME SCALING
+	 worldTrans.getOpenGLMatrix( owner->transform->WorldMatrix().dataArray );
+
+	// TODO: Rotation via Quaternions
+}
