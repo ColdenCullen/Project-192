e4d2fbaabe100d4ce6fa8da1a373e620c1b7f6fa
Colden Cullen <ColdenCullen@gmail.com>
2013-11-21 21:21:54 -0500

    Fixed bug with node install.

--- init.bat
+++ init.bat
@@ -5,7 +5,7 @@ xcopy ExternalDependencies\bin Output\Final\Debug /e /y /i /r
 xcopy ExternalDependencies\bin Output\Final\Release /e /y /i /r
 
 ECHO Initializing NodeJS
-msiexec /i ExternalDependencies\installers\Node.msi /quiet /qn /norestart
+msiexec /i "ExternalDependencies\installers\Node.msi" /quiet /qn /norestart
 start cmd /c "cd Game && init.bat"
 
 pause
