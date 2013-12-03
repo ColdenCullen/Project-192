70939aebdb1c84894f7585149858b0b8e446ea7e
Colden Cullen <ColdenCullen@gmail.com>
2013-12-03 13:52:37 -0500

    Updated init scripts to set GUIDS of lib and exe projects. Still need to add for other 2.

--- init.ps1
+++ init.ps1
@@ -9,15 +9,18 @@ msiexec /i "ExternalDependencies\installers\node\node.msi" /quiet /qn /norestart
 "Initializing game dependencies..."
 Start-Process init.bat -WorkingDirectory "Game"
 
-function Update-Cmake( $file, $guid )
+function Update-Cmake( $file )
 {
     $content = Get-Content $file
     $match = ".*?INCLUDE\(TestBigEndian\).*?"
-    $replacement = "set_property( GLOBAL PROPERTY HLSLcc_GUID `"" + $guid + "`" )`n" +
-                    "get_property( projectGUID GLOBAL PROPERTY HLSLcc_GUID )`n" +
-                    "MESSAGE( STATUS `"Setting project GUID to: ${projectGUID}`")`n" +
-                    "SET( libHLSLcc_GUID_CMAKE `"${projectGUID}`" CACHE INTERNAL `"ProjectGUID`" )`n" +
-                    "SET( HLSLcc_GUID_CMAKE `"${projectGUID}`" CACHE INTERNAL `"ProjectGUID`" )`n" +
+    $replacement =	"set_property( GLOBAL PROPERTY HLSLcc_GUID `"aa6e68b4-fb83-4093-95a5-dddf2b02e198`" )`n" +
+					"set_property( GLOBAL PROPERTY libHLSLcc_GUID `"844FF918-0146-43C2-8765-0BD5B27FB689`" )`n" +
+                    "get_property( exeGUID GLOBAL PROPERTY HLSLcc_GUID )`n" +
+					"get_property( libGUID GLOBAL PROPERTY libHLSLcc_GUID )`n" +
+                    "MESSAGE( STATUS `"Setting exe GUID to: `${exeGUID}`")`n" +
+					"MESSAGE( STATUS `"Setting lib GUID to: `${libGUID}`")`n" +
+                    "SET( HLSLcc_GUID_CMAKE `"`${exeGUID}`" CACHE INTERNAL `"projectGUID`" )`n" +
+                    "SET( libHLSLcc_GUID_CMAKE `"`${libGUID}`" CACHE INTERNAL `"projectGUID`" )`n" +
                     "`nINCLUDE(TestBigEndian)"
 
     if( $content -match $match ) {
@@ -26,5 +29,5 @@ function Update-Cmake( $file, $guid )
 }
 
 "Updating HLSLcc CMake file..."
-Update-Cmake ([System.IO.Path]::GetFullPath( ".\ExternalDependencies\submodules\hlslcc\mk\CMakeLists.txt" )) "aa6e68b4-fb83-4093-95a5-dddf2b02e198"
+Update-Cmake ([System.IO.Path]::GetFullPath( ".\ExternalDependencies\submodules\hlslcc\mk\CMakeLists.txt" ))
 Start-Process -FilePath ".\ExternalDependencies\tools\cmake\bin\cmake" -ArgumentList "-G `"Visual Studio 11`"" -WorkingDirectory ".\ExternalDependencies\submodules\hlslcc\mk" -NoNewWindow -Wait
